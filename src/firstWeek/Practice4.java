34b7ea3fd244789c12be015cd48532a42014988f
zhongtaoaki <zhongtaodylan@163.com>
2022-04-07 18:16:16 +0900

    练习4

--- src/firstWeek/Practice4.java
+++ src/firstWeek/Practice4.java
@@ -6,16 +6,25 @@ package firstWeek;
 public class Practice4 {
 
 	static void sort(int[] array) {
-		
-		
+		for (int i = 0; i < array.length - 1; i++) {
+			for (int j = 0; j < array.length - i - 1; j++) {
+				if (array[j] > array[j + 1]) {
+					int temp = array[j];
+					array[j] = array[j + 1];
+					array[j + 1] = temp;
+				}
+			}
+		}
 	}
-	
+
 	public static void main(String[] args) {
-		
-		int[] array = {3,5,1,2,4};
+
+		int[] array = { 3, 5, 1, 2, 4 };
 		sort(array);
-		
-		//System.out.println();
-		
+
+		for (int i : array) {
+			System.out.println(i);
+		}
+
 	}
 }
