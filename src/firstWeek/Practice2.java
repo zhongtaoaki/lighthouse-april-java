68908fecb9f68982638e736b9dd9e2ba579a898c
zhongtaoaki <zhongtaodylan@163.com>
2022-04-07 16:00:41 +0900

    练习2

--- src/firstWeek/Practice2.java
+++ src/firstWeek/Practice2.java
@@ -1,24 +1,28 @@
 package firstWeek;
 
 /**
- * 有方法add()
- * 参数为两个数字
- * 返回值为这两个数字的和
- * 要求计算
- * 1+1
- * 1+0.1
- * 0.1+1
- * 0.1+0.1
- * 的值
+ * 有方法add() 参数为两个数字 返回值为这两个数字的和 要求计算 1+1 1+0.1 0.1+1 0.1+0.1 的值
  */
 public class Practice2 {
 
-	static int add(int i,int j) {
-		return i+j;
+	static int add(int i, int j) {
+		return i + j;
 	}
-	
+
+	static double add(int i, double j) {
+		return i + j;
+	}
+
+	static double add(double i, int j) {
+		return i + j;
+	}
+
+	static double add(double i, double j) {
+		return i + j;
+	}
+
 	public static void main(String[] args) {
-		
+
 		System.out.println(add(1, 1));
 		System.out.println(add(1, 0.1));
 		System.out.println(add(0.1, 1));
