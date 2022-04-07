723cbcb5bc0bb5f79ab939a0d5337b4789aa3eb2
zhongtaoaki <zhongtaodylan@163.com>
2022-04-07 16:48:38 +0900

    练习3

--- src/firstWeek/Practice3.java
+++ src/firstWeek/Practice3.java
@@ -1,22 +1,28 @@
 package firstWeek;
 
 /**
- * 写一个方法digitSum()
- * 求输入值的各个位数之和
+ * 写一个方法digitSum() 求输入值的各个位数之和
  * 
  */
 public class Practice3 {
 
-	
 	static int digitSum(int num) {
-		
+
+		int sum = 0;
+
+		while (num > 0) {
+			sum += num % 10;
+			num = num / 10;
+		}
+		return sum;
+
 	}
-	
+
 	public static void main(String[] args) {
-		
-		System.out.println(digitSum(10));//1
-		System.out.println(digitSum(25));//7
-		System.out.println(digitSum(828));//18
-		
+
+		System.out.println(digitSum(10));// 1
+		System.out.println(digitSum(25));// 7
+		System.out.println(digitSum(82854));// 18
+
 	}
 }
