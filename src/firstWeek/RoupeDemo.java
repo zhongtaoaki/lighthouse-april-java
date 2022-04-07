140250a587a6955e055b407024d9f801a3dde546
zhongtaoaki <zhongtaodylan@163.com>
2022-04-07 12:57:57 +0900

    循环

--- src/firstWeek/RoupeDemo.java
+++ src/firstWeek/RoupeDemo.java
@@ -1,11 +1,39 @@
 package firstWeek;
+
+import java.util.Iterator;
+
 /**
- * DRY:Dont repeat yourself
+ * DRY:Dont repeat yourself 迭代器 iterator
  */
 public class RoupeDemo {
-    
-    public static void main(String[] args) {
-        
 
-    }
+	public static void main(String[] args) {
+
+		int i = 0;
+
+//		while (i > 10) {
+//			System.out.println(i);
+//			i++;
+//		}
+
+//		do {
+//			System.out.println(i);
+//			i++;
+//		} while (i > 10);
+
+//		for (int j = 0; j < 10; j++) {
+//			System.out.println(j);
+//		}
+		
+		String[] array = {"4","8","10"};
+		for (int j = 0; j < array.length; j++) {
+			System.out.println(array[j]);
+			array[j] = array[j+1];
+		}
+		
+		for (String j : array) {
+			System.out.println(j);
+		}
+		
+	}
 }
