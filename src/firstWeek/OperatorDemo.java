60a554eb73e2947b2369714f99fdf7e6e73f8001
zhongtaoaki <zhongtaodylan@163.com>
2022-04-07 11:21:37 +0900

    运算符和条件分歧

--- src/firstWeek/OperatorDemo.java
+++ src/firstWeek/OperatorDemo.java
@@ -1,16 +1,40 @@
 package firstWeek;
+
 /**
- * 算数运算符
- * 赋值运算符
- * 比较运算符 
- * 逻辑运算符
+ * 算数运算符 +-*%/ ++ -- **
+ * 
+ * 赋值运算符 = 右边的值赋给左边的变量 +=,-=
+ * 
+ * 比较运算符 == != >= <= 结果是boolean （true false）
+ * 
+ * 逻辑运算符 & | ! && ||
+ * 增强代码的健壮性，可用性
+ * 增强代码的运行效率（performance）
  */
 
 public class OperatorDemo {
-    
-    public static void main(String[] args) {
-        
-        
 
-    }
+	public static void main(String[] args) {
+
+		int i = 1;
+		int j = i++;
+		int k = ++i;
+		int l = 17;
+
+		System.out.println(j);
+		System.out.println(i);
+		System.out.println(i++);
+		System.out.println(i);
+		System.out.println(k);
+		System.out.println("17对4取余是: " + l % 4);
+		System.out.println(1 + 1);
+		System.out.println("1" + "1");
+
+		boolean[] booleanArray = {true,false};
+		
+		//boolean b1 = booleanArray[2];
+		boolean b2 = booleanArray[1];
+		System.out.println(b2 && booleanArray[2]);
+
+	}
 }
--- src/firstWeek/ifDemo.java
+++ src/firstWeek/ifDemo.java
@@ -1,13 +1,54 @@
 package firstWeek;
+
 /**
  * if
  * 
  */
 public class ifDemo {
 
-    public static void main(String[] args) {
+	public static void main(String[] args) {
 
+		int i = 0;
 
+		if (i < 0) {
+			System.out.println(i);
+		} else {
+			System.out.println("i比0大或者等于0");
+		}
 
-    }
+		if (i < 0) {
+			System.out.println(i);
+		}
+		System.out.println("i是一个整数");
+
+		// byte short int char 
+		// String Enum(枚举)
+		switch (i) {
+		case 0:
+
+			break;
+
+		case 1:
+
+			break;
+
+		case 2:
+
+			break;
+
+		default:
+			break;
+		}
+
+		if (i == 0) {
+
+		} else if (i == 1) {
+
+		} else if (i == 2) {
+
+		} else {
+
+		}
+
+	}
 }
