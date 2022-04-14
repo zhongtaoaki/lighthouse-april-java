52109358b3a0ee14d1049cfc1acc8397291f279d
zhongtaoaki <zhongtaodylan@163.com>
2022-04-14 16:10:08 +0900

    練習３たたき台修正

--- src/secondWeek/Practice3.java
+++ src/secondWeek/Practice3.java
@@ -1,9 +1,7 @@
 package secondWeek;
 
 /**
- * 使用Practice2的Token完成下列代码
- * 测试类中输入的表达式只包含三个数和两个运算符，且不包含括号
- * 提示：Integer.valueOf();
+ * 使用Practice2的Token完成下列代码 测试类中输入的表达式只包含三个数和两个运算符，且不包含括号 提示：Integer.valueOf();
  * 提示：class Calculator里可以活用switch
  */
 public class Practice3 {
@@ -14,7 +12,7 @@ public class Practice3 {
 		System.out.println(Calculator.calculate("1+1-1"));
 		System.out.println(Calculator.calculate("1+1*1"));
 		System.out.println(Calculator.calculate("1+1/1"));
-		
+
 	}
 
 }
@@ -23,11 +21,10 @@ class Calculator {
 
 	public static int calculate(String expression) {
 
-		int i = Integer.valueOf("1");
-		
-		String s = "1+1+1";
-		char[] chars =  s.toCharArray();
-		
+		int i = Character.getNumericValue('1');
+
+		char[] chars = expression.toCharArray();
+
 	}
 
 }
