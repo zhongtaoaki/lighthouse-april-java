a9c1fcfed9c6df4208469397a4b7f6a41737c7b9
snw2015 <4564959@qq.com>
2022-05-24 18:13:31 +0900

    Add files via upload
    Add permission to the static files

--- src/LoginSecurity/src/main/java/com/example/login/authentication/WebSecurityConfig.java
+++ src/LoginSecurity/src/main/java/com/example/login/authentication/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.*;
@@ -44,5 +45,13 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 				.build();
 
 		return new InMemoryUserDetailsManager(user);
+	}
+	
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+	  web
+	    .debug(false)
+	    .ignoring()
+	    .antMatchers("/images/**", "/js/**", "/css/**");
 	}
 }
